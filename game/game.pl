9de875c69b5bea92d3ef5b032eb10b8235abdc90
Amgad <amgadhammash1@gmail.com>
2022-06-22 17:55:12 +0200

    demo is ready

--- game/game.pl
+++ game/game.pl
@@ -4,8 +4,14 @@
 :- retractall(at(_, _)), retractall(i_am_at(_)), retractall(alive(_)), retractall(boogie_bombable(_)),
         retractall(looked_into_hole(_)), 
         retractall(looked_into_bus(_)), 
-        retractall(wants_to_distract_emp(_)),
-        retractall(moners_in_da_bag(_)).
+        retractall(wants_to_distract_emp(_)),,
+        retractall(employee_is_boogy_bombed(_))
+        retractall(moners_in_da_bag(_)),
+        retractall(boxing_machine_looted(_)),
+        retractall(face_off_looted(_)),
+        retractall(arcade_looted(_)),
+        retractall(whack_a_mole_looted(_)),
+        retractall(over_complicated_boolean_empBombed_and_inCheckout(_)).
 
 /* egg */
 
@@ -20,6 +26,22 @@ getting_da_bag_quest(0).
 
 :- dynamic moners_in_da_bag/1.
 moners_in_da_bag(0).
+
+:- dynamic over_complicated_boolean_empBombed_and_inCheckout/1.
+over_complicated_boolean_empBombed_and_inCheckout(0).
+/*:- dynamic add_moners_in_da_bag/1.*/
+
+:- dynamic boxing_machine_looted/1.
+boxing_machine_looted(0).
+
+:- dynamic face_off_looted/1.
+face_off_looted(0).
+
+:- dynamic arcade_looted/1.
+arcade_looted(0).
+
+:- dynamic whack_a_mole_looted/1.
+whack_a_mole_looted(0).
 
 :- dynamic wants_to_distract_emp/1.
 wants_to_distract_emp(0).
@@ -49,8 +71,26 @@ set_moners_in_da_bag(Money) :-
 
 add_moners_in_da_bag(Money) :-
         moners_in_da_bag(Prev_Money),
-        Temp = Money + Prev_Money,
-        set_moners_in_da_bag(Temp).
+        Temp is Money + Prev_Money,
+        set_moners_in_da_bag(Temp),
+        Temp >= 20,
+        set_getting_da_bag_quest_finished.
+
+set_boxing_machine_looted_true :-
+        retractall(boxing_machine_looted(_)),
+        assert(boxing_machine_looted(1)).
+
+set_face_off_looted_true :-
+        retractall(face_off_looted(_)),
+        assert(face_off_looted(1)).
+
+set_arcade_looted_true :-
+        retractall(arcade_looted(_)),
+        assert(arcade_looted(1)).
+
+set_whack_a_mole_looted_ture :-
+        retractall(whack_a_mole_looted(_)),
+        assert(whack_a_mole_looted(1)).
 
 set_wants_to_distract_emp_true :-
         retractall(wants_to_distract_emp(_)),
@@ -60,10 +100,31 @@ set_employee_is_boogy_bombed_true :-
         retractall(employee_is_boogy_bombed(_)),
         assert(employee_is_boogy_bombed(1)).
 
+set_employee_is_boogy_bombed_balled_out :-
+        retractall(employee_is_boogy_bombed(_)),
+        assert(employee_is_boogy_bombed(2)).
+
+set_over_complicated_boolean_empBombed_true :-
+        over_complicated_boolean_empBombed_and_inCheckout(Bool),
+        Temp is Bool + 1,
+        retractall(over_complicated_boolean_empBombed_and_inCheckout(_)),
+        assert(over_complicated_boolean_empBombed_and_inCheckout(Temp)).
+
+set_over_complicated_boolean_inCheckout_true :-
+        over_complicated_boolean_empBombed_and_inCheckout(Bool),
+        Temp is Bool + 1,
+        retractall(over_complicated_boolean_empBombed_and_inCheckout(_)),
+        assert(over_complicated_boolean_empBombed_and_inCheckout(Temp)).
+
+set_over_complicated_boolean_inCheckout_false :-
+        over_complicated_boolean_empBombed_and_inCheckout(Bool),
+        Temp is Bool - 1,
+        retractall(over_complicated_boolean_empBombed_and_inCheckout(_)),
+        assert(over_complicated_boolean_empBombed_and_inCheckout(Temp)).
+
 /* This defines my current location. */
 
 i_am_at(bedroom).
-
 
 /* These facts describe how the rooms are connected. */
 
@@ -77,15 +138,30 @@ path(battle_bus, n, street).
 
 path(arcade_entrance, e, street).
 path(arcade_entrance, n, game_room).
-path(arcade_entrance, w, checkout) :- at(boogie_bomb, in_hand).
 path(arcade_entrance, w, checkout) :-
+        employee_is_boogy_bombed(Bool),
+        Bool >= 2,
+        look(checkout2),
+        set_over_complicated_boolean_inCheckout_true,!.
+path(arcade_entrance, w, checkout) :- 
+        employee_is_boogy_bombed(Bool),
+        Bool = 1,
+        look(checkout2),
+        set_employee_is_boogy_bombed_balled_out,
+        set_over_complicated_boolean_inCheckout_true,!.
+path(arcade_entrance, w, checkout) :-
+        employee_is_boogy_bombed(Bool),
+        Bool = 0,
         look(checkout), nl,
         !, fail.
 
 path(game_room, s, arcade_entrance).
 
-path(checkout, e, arcade_entrance).
-path(checkout, n, emp_room) :- at(emp_room_key, in_hand).
+path(checkout, e, arcade_entrance) :-
+        set_over_complicated_boolean_inCheckout_false.
+path(checkout, n, emp_room) :- 
+        at(emp_room_key, in_hand),
+        set_over_complicated_boolean_inCheckout_false.
 path(checkout, n, emp_room) :- 
         write('Door is locked :('), nl,
         !, fail.
@@ -96,6 +172,86 @@ path(checkout, n, emp_room) :-
 at(employee, checkout).
 at(boogie_bomb, battle_bus).
 at(emp_room_key, employee_pocket).
+
+/* These facts tell what machines can be seached to find money*/
+/*boxing machine
+ face-off dance battle game, 
+ arcade machine, 
+ whack a mole */
+is_searchable_game(boxing_machine).
+is_searchable_game(face_off).
+is_searchable_game(arcade).
+is_searchable_game(whack_a_mole).
+
+/* These rules add coins to the players inventory */
+
+search(Game) :-
+        is_searchable_game(Game),
+        game_searched(Game).
+
+
+game_searched(boxing_machine) :-
+        boxing_machine_looted(Bool),
+        Bool = 0,
+        write('You try to see if any money is left in the machine, but then the punching bag'), nl,
+        write('releases and hits you in the face. As you fall back, you hold on the punching bag'), nl,
+        write('and cause the whole machine to fall with you.'), nl,
+        write('9 coins fall out'),
+        add_moners_in_da_bag(9),
+        set_boxing_machine_looted_true,
+        !.
+
+game_searched(boxing_machine) :-
+        boxing_machine_looted(Bool),
+        Bool >= 1,
+        write('The machine lays on the ground infront of you...'),nl,
+        write('Better get going'),!.
+
+game_searched(face_off) :-
+        face_off_looted(Bool),
+        Bool = 0,
+        write('As you search the dance-off machine, it suddenly turns on and you simply had to dance,'), nl,
+        write('After a good face-off session, you hear the cling noise of coins falling out of the machine'), nl,
+        write('Jackpot, 21 dabloons (coins)'),
+        add_moners_in_da_bag(21),
+        set_face_off_looted_true,
+        !.
+
+game_searched(face_off) :-
+        face_off_looted(Bool),
+        Bool >= 1,
+        write('You are still sweating from your last face-off session, perhaps its enough for today.'),!.
+
+game_searched(arcade) :-
+        arcade_looted(Bool),
+        Bool = 0,
+        write('Someone seems to have left street fighter running on it, they have even placed a bet too!'), nl,
+        write('After defeating the enemy (by that winning the bet that the previous player has set) you feel proud of yourself'), nl,
+        write('Nice, you just earned yourself 12 coins!'),
+        add_moners_in_da_bag(12),
+        set_arcade_looted_true,
+        !.
+
+game_searched(arcade) :-
+        arcade_looted(Bool),
+        Bool >= 1,
+        write('You are not much of a gambler yourself, you dont want to stick any of your money into this machine.'),!.
+
+game_searched(whack_a_mole) :-
+        whack_a_mole_looted(Bool),
+        Bool = 0,
+        write('As you inspect the machine the annoying moles keep popping up and go back into their hidey hole.'), nl,
+        write('It begins to frustrate you so you take the hammer and take stopping them into your own hand.'), nl,
+        write('After smashing your frustration away at the machine, 5 coind pop out.'),
+        add_moners_in_da_bag(5),
+        set_whack_a_mole_looted_ture,
+        !.
+
+game_searched(whack_a_mole) :-
+        whack_a_mole_looted(Bool),
+        Bool >= 1,
+        write('The machine is barely standing, sparks flying out of it in every direction.'),nl,
+        write('You moonslide away...').
 
 /* These rules describe how to pick up an object. */
 
@@ -170,9 +326,11 @@ look :-
 
 look(Place) :-
         Place \= battle_bus,
+        over_complicated_boolean_empBombed_and_inCheckout(Bool),
+        Bool =< 1,
         describe(Place), nl,
         notice_objects_at(Place),
-        nl.
+        nl,!.
 
 look(battle_bus) :-
         describe(battle_bus),
@@ -184,7 +342,7 @@ look_inside :-
         wants_to_distract_emp(Bool),
         Bool = 0,
         write('A hand reaches out and blocks you from looking into the bus.'), nl,
-        write('"Didnt ya parents teach ya not to poke yer nose around other peoples stuff, brat?"'), nl.
+        write('"Didnt ya parents teach ya not to poke yer nose around other peoples stuff, brat?"'), nl,!.
 
 look_inside :-
         i_am_at(battle_bus),
@@ -204,7 +362,7 @@ look_inside :-
         write('You take a step back in shock'), nl,
         write('"Dont soil yer pants, youngin, this not a real bomb, it only forces people to BOOGIE DOWN, HA HA HA"'), nl,
         write('He shows you the bomb, but doesnt give it to you'), nl,
-        write('"Nuthin ere is free, bring me 20 coins and it be yers"'), nl.
+        write('"Nuthin ere is free, bring me 20 coins and it be yers"'), nl,!.
 
 look_inside :-
         i_am_at(battle_bus),
@@ -214,7 +372,7 @@ look_inside :-
         Bool >= 1,
         getting_da_bag_quest(Started),
         Started = 1,
-        write('"Nuthin ere is free, bring me 20 coins and it be yers..."'), nl.
+        write('"Nuthin ere is free, bring me 20 coins and it be yers..."'), nl,!.
 
 look_inside :-
         i_am_at(battle_bus),
@@ -224,7 +382,20 @@ look_inside :-
         Bool >= 1,
         getting_da_bag_quest(Started),
         Started = 1,
-        write('"Nuthin ere is free, bring me 20 coins and it be yers..."'), nl.
+        write('"Nuthin ere is free, bring me 20 coins and it be yers..."'), nl,!.
+
+look_inside :-
+        i_am_at(battle_bus),
+        looked_into_bus(Times),
+        Times >= 1,
+        wants_to_distract_emp(Bool),
+        Bool >= 1,
+        getting_da_bag_quest(Ended),
+        Ended = 2,
+        write('"Oooooh, i can smell the COINS from in here."'), nl,
+        write('"Here ya go, yer bomb"'),nl,
+        write('Boogie-Bomb added to you inventory, write "throwBomb" when near the employee'),
+        take(boogie_bomb),!.
 
 
 /* This is also to look, but for easter egg*/
@@ -232,11 +403,10 @@ look_inside :-
 stare_into_hole :-
         i_am_at(bedroom),
         looked_into_hole(Count),
-        Count >= 3,
+        Count = 3,
         Temp is Count + 1,
         set_looked_into_hole_count(Temp),
-        write(Temp),nl,
-        write("black people be like"),!.
+        write("Wait, i think i just saw something slither away... :("),!.
 
 
 stare_into_hole :-
@@ -245,11 +415,18 @@ stare_into_hole :-
         Count < 3,
         Temp is Count + 1,
         set_looked_into_hole_count(Temp),
-        write("hmm sus"), nl,
+        write("Nothing to see"), nl,
         write(Temp), nl.
         /*Temp > 3,
         write("black people be like").*/
 
+stare_into_hole :-
+        i_am_at(bedroom),
+        looked_into_hole(Count),
+        Count > 3,
+        Temp is Count + 1,
+        set_looked_into_hole_count(Temp),
+        write("Meh, probably nothing"),!.
 
 /* These rules set up a loop to mention all the objects
    in your vicinity. */
@@ -264,14 +441,15 @@ notice_objects_at(_).
 /* These rules tell how to handle killing the lion and the spider. */
 
 throwBomb :-
-        i_am_at(checkout),
+        i_am_at(arcade_entrance),
         at(boogie_bomb, in_hand),
-        retract(boogie_bombable(employee)),
-        write('You boogie bombed the employee'), nl, 
+        set_employee_is_boogy_bombed_true,
+        set_over_complicated_boolean_empBombed_true,
+        write('You boogie bombed the employee, now he is dancing and he cant get in your way anymore!'), nl, 
         !.
 
 throwBomb :-
-        i_am_at(checkout),
+        i_am_at(arcade_entrance),
         write('Cant throw something you dont have, man'), nl, 
         !.
 
@@ -377,6 +555,13 @@ describe(checkout) :-
         write('Behind the counter, you see a door labeled "Employees Only" which seems very intriguing'), nl, 
         write('Try to get behind the counter? (get_behind_counter)'), nl,
         set_wants_to_distract_emp_true, !.
+
+describe(checkout2) :-
+        employee_is_boogy_bombed(Bool),
+        Bool >= 1,
+        write('The checkout looks disgusting from the inside, you cant believe that the kids would'),nl,
+        write('really stick the vampire teeth, that have been exposed to all this filth, into their mouths'),nl,
+        write('Anyway, back to your original plan, finding the secret held behind the employees door.'),!.
 
 describe(cave) :-
         write('Yecch!  There is a giant spider here, twitching.'), nl.
